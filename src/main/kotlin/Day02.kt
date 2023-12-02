eb628281bc564d179862ca6beca36b22c1cd8d3f
Ruben Groot Nibbelink <ruben.grootnibbelink@gmail.com>
2023-12-02 15:19:39 +0100

    Day 02 (bigger refactor)

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -5,23 +5,34 @@ fun main() {
     part2()
 }
 
-fun part1() = File("src/main/resources/02.txt").readLines().map { it.split(":") }
-    .associate { it[0].key() to it[1].value() }
-    .filter { (_, value) ->
-        value.all {
-            (it["red"] ?: 0) <= 12 && (it["green"] ?: 0) <= 13 && (it["blue"] ?: 0) <= 14
-        }
-    }
-    .map { (key, _) -> key }
+fun part1() = parseFromFile()
+    .filter { (_, value) -> value.hasEnoughOfEachColor() }
+    .keys
     .sum()
     .let { println("part 1: $it") }
 
-fun part2() = File("src/main/resources/02.txt").readLines().map { it.split(":") }
-    .map { it[1].value() }
-    .sumOf { game ->
-        game.maxOf { it["red"] ?: 0 } * game.maxOf { it["green"] ?: 0 } * game.maxOf { it["blue"] ?: 0 }
-    }
+fun part2() = parseFromFile().values
+    .sumOf { it.max("red") * it.max("green") * it.max("blue") }
     .let { println("part 2: $it") }
+
+private fun List<Map<String, Int>>.hasEnoughOfEachColor() = this.all {
+    it.hasEnough("red") && it.hasEnough("green") && it.hasEnough("blue")
+}
+
+private fun Map<String, Int>.hasEnough(color: String) =
+    (this[color] ?: 0) <= colors[color]!!
+
+private val colors = mapOf(
+    "red" to 12,
+    "green" to 13,
+    "blue" to 14
+)
+
+private fun List<Map<String, Int>>.max(color: String) =
+    this.maxOf { it[color] ?: 0 }
+
+private fun parseFromFile() = File("src/main/resources/02.txt").readLines().map { it.split(":") }
+    .associate { it[0].key() to it[1].value() }
 
 private fun String.value(): List<Map<String, Int>> = split(";")
     .map { round ->
