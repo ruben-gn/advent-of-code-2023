241e543c8f7456523ffe071b274afd161b87696b
Ruben Groot Nibbelink <ruben.grootnibbelink@gmail.com>
2023-12-09 09:23:20 +0100

    Day 08 (thank you baeldung)

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -16,6 +16,12 @@ private fun part1() {
     println("part 1: $steps steps")
 }
 
+private fun part2() {
+    val steps = nodes.keys.filter { it.endsWith("A") }.toSet().map { calculateStepsFromTo(it) { it.endsWith("Z") } }
+    val result = findLCMOfListOfNumbers(steps.map { it.toLong() })
+    println("part 2: $result steps")
+}
+
 private fun calculateStepsFromTo(start: String, endCondition: (String) -> Boolean): Int {
     var node = start
     var steps = 0
@@ -28,12 +34,23 @@ private fun calculateStepsFromTo(start: String, endCondition: (String) -> Boolea
     return steps
 }
 
-private fun part2() {
-    val steps = nodes.keys.filter { it.endsWith("A") }.toSet().map { calculateStepsFromTo(it) { it.endsWith("Z") } }
-    val result = findLCMOfListOfNumbers(steps.map { it.toLong() })
-    println("part 2: $result steps")
+private fun Pair<String, String>.take(side: String) = when (side) {
+    "L" -> this.first
+    "R" -> this.second
+    else -> throw IllegalArgumentException("Invalid side: $side")
 }
 
+private fun List<String>.notEmpty() = this.filter { it != "" }
+
+private fun List<String>.extractNodes() =
+    with(
+        this[1]
+            .replace("(", "")
+            .replace(")", "")
+            .split(", ")
+    ) { this[0] to this[1] }
+
+// LCM code shamelessly copy-pasted from baeldung
 fun findLCMOfListOfNumbers(numbers: List<Long>): Long {
     var result = numbers[0]
     for (i in 1 until numbers.size) {
@@ -54,19 +71,3 @@ fun findLCM(a: Long, b: Long): Long {
     }
     return maxLcm
 }
-
-private fun Pair<String, String>.take(side: String) = when (side) {
-    "L" -> this.first
-    "R" -> this.second
-    else -> throw IllegalArgumentException("Invalid side: $side")
-}
-
-private fun List<String>.notEmpty() = this.filter { it != "" }
-
-private fun List<String>.extractNodes() =
-    with(
-        this[1]
-            .replace("(", "")
-            .replace(")", "")
-            .split(", ")
-    ) { this[0] to this[1] }
